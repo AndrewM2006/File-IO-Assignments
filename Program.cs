194342a247e131c9a27747b9927948a17ecd0b30
Andrew Monteith <339531824@GoTVDSB.ca>
2023-10-24 10:50:49 -0400

    finished tutorial

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +13,27 @@ namespace File_IO_Assignments
     {
         static void Main(string[] args)
         {
-
+            StreamWriter writer1 = new StreamWriter("../../sortedNumbers.txt");
+            StreamWriter writer2 = new StreamWriter("../../numberSummary.txt");
+            List <int> numbers = new List<int>();
+            int sum=0, average;
+            foreach (string line in File.ReadLines(@"numbers.txt", Encoding.UTF8))
+            {
+                numbers.Add(Convert.ToInt32(line));
+            }
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            average= sum / numbers.Count;
+            numbers.Sort();
+            foreach (int number in numbers)
+            {
+                writer1.WriteLine(number);
+            }
+            writer1.Close();
+            writer2.WriteLine($"The sum of the numbers is {sum} and the average is {average}!");
+            writer2.Close();
         }
     }
 }
