8b55be2dcb4970efc8c8ee359b092e1729a5a568
Andrew Monteith <339531824@GoTVDSB.ca>
2023-10-25 11:02:20 -0400

    this is done however I want to use get avergae so I'll probably add something friday

---
+++ EventScore.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO_Assignments
+{
+    class EventScore : IComparable<EventScore>
+    {
+        private string _name;
+        private string _event;
+        private List<double> _score;
+
+        public EventScore (string  name, string talent, List<double> Score)
+        {
+            this._name = name.Trim();
+            this._event = talent.Trim();
+            this._score = Score;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public string Event
+        {
+            get
+            {
+                return _event;
+            }
+            set
+            {
+                this._event = value;
+            }
+        }
+
+        public List<double> Score
+        {
+            get
+            {
+                return _score;
+            }
+            set
+            {
+                this._score = value;
+            }
+        }
+
+        public double GetTotalScore()
+        {
+            return Score.Sum();
+        }
+
+        public double GetAverage()
+        {
+            return Score.Average();
+        }
+
+        public override string ToString()
+        {
+            return _name + " showed skills in " + _event + " and recieved a score of " + GetTotalScore() + "/50";
+        }
+
+        public int CompareTo(EventScore that)
+        {
+            return this ._score.Sum().CompareTo(that._score.Sum());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,27 +13,56 @@ namespace File_IO_Assignments
     {
         static void Main(string[] args)
         {
-            StreamWriter writer1 = new StreamWriter("../../sortedNumbers.txt");
-            StreamWriter writer2 = new StreamWriter("../../numberSummary.txt");
-            List <int> numbers = new List<int>();
-            int sum=0, average;
-            foreach (string line in File.ReadLines(@"numbers.txt", Encoding.UTF8))
+            List <EventScore> eventScores = new List <EventScore> ();
+            List <double> scores = new List <double> ();
+            List <string> results = new List <string> ();
+            string name, eventName, menuOption;
+            int currentIndex = 0;
+            foreach (string line in File.ReadLines(@"results.txt", Encoding.UTF8))
             {
-                numbers.Add(Convert.ToInt32(line));
+                results.Add (line);
             }
-            foreach (int number in numbers)
+            while (currentIndex < results.Count)
             {
-                sum += number;
+                name = results[currentIndex];
+                eventName = results[currentIndex + 1];
+                scores = results
+                    .Skip(currentIndex + 2)
+                    .Take(5)
+                    .Select(score => double.TryParse(score, out double parsedScore) ? parsedScore : 0.0)
+                    .ToList();
+                EventScore eventScore = new EventScore(name, eventName, scores);
+                eventScores.Add (eventScore);
+                currentIndex += 7;
             }
-            average= sum / numbers.Count;
-            numbers.Sort();
-            foreach (int number in numbers)
+            while (true)
             {
-                writer1.WriteLine(number);
+                Console.WriteLine("Press 1 to Print Scores");
+                Console.WriteLine("Press 2 to Print Highest Score");
+                Console.WriteLine("Press 3 to Print Lowest Score");
+                Console.WriteLine("Press Q for Quit");
+                menuOption = Console.ReadLine();
+                eventScores.Sort();
+                if (menuOption == "1")
+                {
+                    foreach (EventScore eventScore in eventScores)
+                    {
+                        Console.WriteLine(eventScore);
+                    }
+                }
+                else if (menuOption == "2")
+                {
+                    Console.WriteLine(eventScores[eventScores.Count()-1]);
+                }
+                else if (menuOption == "3")
+                {
+                    Console.WriteLine(eventScores[0]);
+                }
+                else if (menuOption == "q" || menuOption == "Q")
+                {
+
+                }
             }
-            writer1.Close();
-            writer2.WriteLine($"The sum of the numbers is {sum} and the average is {average}!");
-            writer2.Close();
         }
     }
 }
